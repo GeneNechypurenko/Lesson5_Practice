53b7fba727376aeb465bef3090a940cdc7608a6f
Eugene Nechypurenko <e.nechypurenko@gmail.com>
2023-11-22 20:42:23 +0200

    Add project files.

---
+++ Lesson5_Practice/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Lesson5_Practice
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private NewWindow newWindow;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void openWindowButton_Click(object sender, RoutedEventArgs e)
+        {
+            newWindow = new NewWindow();
+            newWindow.Show();
+        }
+
+        private void closeWindowButton_Click(object sender, RoutedEventArgs e)
+        {
+            newWindow?.Close();
+        }
+    }
+}
